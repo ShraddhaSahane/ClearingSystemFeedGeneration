4cc2b418e633c65dfb2c07b98df15e72ce484669
mrugagokhale28 <56423069+mrugagokhale28@users.noreply.github.com>
2021-04-14 12:40:53 +0530

    Update TransactionService.java

--- TransactionService.java
+++ TransactionService.java
@@ -1,5 +1,10 @@
 package com.shraddha.service;
+import java.sql.SQLException;
+	import java.util.ArrayList;
+
+	import com.shraddha.bean.*;
 
 public interface TransactionService {
-
+ArrayList<Transaction> getAllTransactionRecords() throws ClassNotFoundException ,SQLException;
+		
 }

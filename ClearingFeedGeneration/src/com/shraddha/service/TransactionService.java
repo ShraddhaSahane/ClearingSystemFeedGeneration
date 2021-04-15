4db096ba68c6c665472054c25abcde96e2e9832b
ShraddhaSahane <56163034+ShraddhaSahane@users.noreply.github.com>
2021-04-15 12:03:27 +0530

    Update TransactionService.java

--- ClearingFeedGeneration/src/com/shraddha/service/TransactionService.java
+++ ClearingFeedGeneration/src/com/shraddha/service/TransactionService.java
@@ -1,5 +1,12 @@
 package com.shraddha.service;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
-public interface TransactionService {
-
+import com.shraddha.bean.Transaction;
+public interface TransactionService 
+{
+	ArrayList<Transaction> getAllTransactions() throws ClassNotFoundException ,SQLException;
+	Transaction searchTransactionRecordById(int transactionId)throws ClassNotFoundException ,SQLException;
+	Transaction convertUpperCase(Transaction t);
+	boolean addTransaction(Transaction t) throws ClassNotFoundException, SQLException;
 }

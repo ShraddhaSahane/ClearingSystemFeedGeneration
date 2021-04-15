5545e78d07bd3ba21e3f8f61790321904e8762fa
ShraddhaSahane <56163034+ShraddhaSahane@users.noreply.github.com>
2021-04-15 12:48:44 +0530

    Update TransactionMain.java

--- ClearingFeedGeneration/src/com/shraddha/client/TransactionMain.java
+++ ClearingFeedGeneration/src/com/shraddha/client/TransactionMain.java
@@ -1,5 +1,20 @@
 package com.shraddha.client;
+import java.util.Scanner;
+import com.shraddha.presentation.*;
 
-public class TransactionMain {
+public class TransactionMain 
+{
+	public static void main(String[] args) {
+		TransactionPresentation transactionPresentation=new TransactionPresentationImpl();
+	
+		Scanner scanner=new Scanner(System.in);
+		while(true) {
+			transactionPresentation.showMenu();
+			System.out.println("Enter Choice : ");
+			int choice=scanner.nextInt();
+			transactionPresentation.performMenu(choice);
+			
+		}
 
+	}
 }

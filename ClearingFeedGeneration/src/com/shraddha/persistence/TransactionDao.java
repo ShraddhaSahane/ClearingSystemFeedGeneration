096837c01c0b9d1cc4ebc5ed62f1434cb7a4ec43
ShraddhaSahane <56163034+ShraddhaSahane@users.noreply.github.com>
2021-04-17 12:34:54 +0530

    Update TransactionDao.java

--- ClearingFeedGeneration/src/com/shraddha/persistence/TransactionDao.java
+++ ClearingFeedGeneration/src/com/shraddha/persistence/TransactionDao.java
@@ -8,5 +8,5 @@ public interface TransactionDao {
 	public ArrayList<Transaction> getAllTransactionRecords() throws ClassNotFoundException ,SQLException;
 	public boolean insertTransactionRecord(Transaction t)throws ClassNotFoundException ,SQLException;
 	Transaction searchTransactionById(long transactionId)throws ClassNotFoundException ,SQLException;
-
+	public void validateTransaction() throws ClassNotFoundException ,SQLException;
 }
